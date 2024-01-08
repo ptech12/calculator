1f020cf8830a9725db3df2e2cc13822846e90834
Prasanth A R <73158535+ptech12@users.noreply.github.com>
2024-01-08 21:42:11 +0530

    appending number working

--- script.js
+++ script.js
@@ -1,1 +1,67 @@
-// variable
+// Calc class
+class Calculator{
+    constructor(previousOperandTextElement, currentOperandTextElement){
+        this.previousOperandTextElement = previousOperandTextElement;
+        this.currentOperandTextElement = currentOperandTextElement;
+    }
+
+    // to clear ()
+    clear(){
+        this.currentOperand = '';
+        this.previousOperand = '';
+        this.operation = undefined;
+    }
+
+    // DEL
+    delete(){
+
+    }
+
+    // appends to output screen
+    appendNumber(number){
+        this.currentOperand = (this.currentOperand ? this.currentOperand.toString() : ' ')  + number.toString();
+
+    }
+
+    // chooses the specified operation by the user
+    chooseOperation(operation) {
+
+    }
+
+    // computes the value based on the two numbers
+    compute(){
+
+    }
+
+    // to update the display after the compute
+    updateDisplay(){
+        this.currentOperandTextElement.innerText = this.currentOperand
+
+    }
+
+}
+
+
+
+
+
+// variables
+
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const equalsButton = document.querySelector('[data-equals]');
+const deleteButton = document.querySelector('[data-delete]');
+const allClearButton = document.querySelector('[data-all-clear]');
+const previousOperandTextElement = document.querySelector('[data-previous-operand]');
+const currentOperandTextElement = document.querySelector('[data-current-operand]');
+
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement);
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText);
+        calculator.updateDisplay();
+
+    })
+})
